58654e45c75446e706de7020d37c1f4a9c7761d4
vipcchua <vipcchua@hotmail.com>
2017-01-24 15:08:34 +0800

    Spring-Cloud
    Spring-Cloud

---
+++ Spring-Cloud-Ribbon/src/main/java/com/CchuaSpace/Service/ComputeClient.java
@@ -0,0 +1,15 @@
+package com.CchuaSpace.Service;
+
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("Server-Service")
+public interface ComputeClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+
+}
---
+++ Spring-Cloud-Server/src/main/java/com/CchuaSpace/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.CchuaSpace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ServerApplication.class).web(true).run(args);
+	}
+
+}
