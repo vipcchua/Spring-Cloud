90e25fb7a8c4bb600d73b41075642cf644294eb3
Cchua <vipcchua@hotmail.com>
2017-02-12 17:38:32 +0800

    Spring Cloud 服务网关
    Spring Cloud 服务网关

---
+++ Spring-Cloud-Api-Gateway/src/main/java/com/CchuaSpace/Application.java
@@ -0,0 +1,23 @@
+package com.CchuaSpace;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.CchuaSpace.Filter.AccessFilter;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class Application {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+
+}
--- Spring-Cloud-Server/src/main/java/com/CchuaSpace/ServerApplication.java
+++ Spring-Cloud-Server/src/main/java/com/CchuaSpace/ServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+
 @SpringBootApplication
 public class ServerApplication {
 
